22c26c13fe732fc59e38c5760145bf11db563dae
markgit666 <943121438@qq.com>
2019-07-22 23:03:44 +0800

    vue项目-first commit

---
+++ src/views/Info.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>info页面
+    <button type="button" @click="add()">添加</button>
+  </div>
+</template>
+
+<script>
+  import store from '@/store';
+
+  export default {
+    name: 'info',
+    store,
+    methods: {
+      add() {
+        store.commit("increase")
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
